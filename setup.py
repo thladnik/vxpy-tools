732497d54e041b16bd13e5df90ac5fbf939ea36f
thladnik <tim.hladnik@gmail.com>
2022-11-07 11:03:06 +0100

    Move ProcessProxy init to forks; add docstrings to core; start adding protocol info to container file;

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vxpy-tools',
+    version='0.0.1',
+    packages=['vxpy-tools', 'vxpy-tools.summarize'],
+    url='',
+    license='',
+    author='thladnik',
+    author_email='',
+    description=''
+)
