ee2b0a2f2ac53880cb2d524bd0ae563abd698c2b
thladnik <tim.hladnik@gmail.com>
2022-11-08 16:50:47 +0100

    Fix package name;

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+with open('requirements.txt', 'r') as f:
+    install_deps = f.readlines()
+
 setup(
-    name='vxpy-tools',
+    name='vxpy_tools',
     version='0.0.1',
     packages=['vxpy-tools', 'vxpy-tools.summarize'],
     url='',
     license='',
+    install_requires=install_deps,
     author='thladnik',
     author_email='',
     description=''
